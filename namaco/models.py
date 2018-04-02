f8c1308315d652655a39acff454257221a37574c
Hironsan <light.tree.1.13@gmail.com>
2018-04-02 11:23:50 +0900

    Update model

--- namaco/models.py
+++ namaco/models.py
@@ -23,24 +23,24 @@ class BaseModel(object):
     def save_params(self, file_path):
         with open(file_path, 'w') as f:
             params = {name.lstrip('_'): val for name, val in vars(self).items()
-                      if name not in {'_loss', 'model'}}
+                      if name not in {'_embeddings', '_loss', 'model'}}
             json.dump(params, f, sort_keys=True, indent=4)
 
     @classmethod
     def load(cls, weights_file, params_file):
-        model = cls.load_params(params_file)
-        model.build_model()
-        model.load_weights(weights_file)
+        params = cls.load_params(params_file)
+        self = cls(**params)
+        self.build()
+        self.load_weights(weights_file)
 
-        return model
+        return self
 
     @classmethod
     def load_params(cls, file_path):
         with open(file_path) as f:
             params = json.load(f)
-            self = cls(**params)
 
-        return self
+        return params
 
     def __getattr__(self, name):
         return getattr(self.model, name)
